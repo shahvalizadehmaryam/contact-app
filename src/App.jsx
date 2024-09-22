32d4a5640556b7428a02d0ad1ae98db423cf1352
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2024-09-22 08:52:06 +0330

    react router dom added to project and contacts comp.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import Contacts from "./components/Contacts";
 
 function App() {
-
   return (
     <>
-
+      <Routes>
+        <Route path="/" element={<Contacts />} />
+        {/* <Route path="/bio" element={<Bio />} /> */}
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Contacts.jsx
@@ -0,0 +1,7 @@
+const Contacts = () => {
+    return ( 
+        <div>Contacts list</div>
+     );
+}
+ 
+export default Contacts;
